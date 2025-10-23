3a1d538ae14fbf0e00351a0d6d308d0357c3e3a9
Henrique de Abreu Pereira <hpereira986@gmail.com>
2025-10-23 00:39:52 -0300

    fix(bug): items not getting added correctly

--- scripts/util/inventory/inventory_manager.gd
+++ scripts/util/inventory/inventory_manager.gd
@@ -75,23 +75,22 @@ func add_new_stack(new_stack: ItemStack, body: Node3D) -> void:
 		respawn_item(new_stack, body)
 	
 
-func add_to_empty_slot(available_slot_index: int, new_stack: ItemStack, array: Array[ItemStack], ui_signal: Signal) -> int:
+func add_to_empty_slot(available_slot_index: int, new_stack: ItemStack, array: Array[ItemStack], ui_signal: Signal) -> void:
 	array[available_slot_index] = new_stack
 	ui_signal.emit(array)
-	return 0
 
 func try_to_add_stack(available_slot_index: int, array: Array[ItemStack], ui_signal: Signal) -> void:
 	var max_stack : int = array[available_slot_index].item_data.max_stack
 	var quantity_sum : int = leftover_quantity + array[available_slot_index].quantity
 	
 	if quantity_sum <= max_stack:
-		array[available_slot_index].quantity = quantity_sum
 		leftover_quantity = 0
+		array[available_slot_index].quantity = quantity_sum
 		ui_signal.emit(array)
 	else:
 		## Found but not enough space
-		array[available_slot_index].quantity = max_stack
 		leftover_quantity  = abs(quantity_sum - max_stack)
+		array[available_slot_index].quantity = max_stack
 		ui_signal.emit(array)
 
 func try_to_add_to_inventory(stack_to_add: ItemStack, array: Array[ItemStack], update_signal: Signal) -> void:
@@ -100,12 +99,16 @@ func try_to_add_to_inventory(stack_to_add: ItemStack, array: Array[ItemStack], u
 	
 	available_slot_index = find_available_slot(array, item_path)
 	
-	if available_slot_index > -1:
+	while(available_slot_index > -1):
 		try_to_add_stack(available_slot_index, array, update_signal)
-		return
+		available_slot_index = find_available_slot(array, item_path)
 	
+	if leftover_quantity == 0: return
+	
+	stack_to_add.quantity = leftover_quantity
 	available_slot_index = find_first_empty(array)
 	
 	if available_slot_index > -1:
+		leftover_quantity = 0
 		add_to_empty_slot(available_slot_index, stack_to_add, array, update_signal)
 #endregion
